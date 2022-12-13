086998368371af110945dd4c941be21fd7c3830e
joris-landaret <joris.landaret@laplateforme.io>
2022-12-13 10:44:58 +0100

    ajout condition pour ajouter données dans la bdd

--- reservation-form.php
+++ reservation-form.php
@@ -14,40 +14,23 @@ var_dump($request_fetch_all);
 if(isset($_POST['envoi'])){
     
     //si les champs sont remplis
-    if($_POST['titre'] && $_POST['debut'] && $_POST['fin'] && $_POST['fin']){
+    if($_POST['titre'] && $_POST['debut'] && $_POST['fin'] && $_POST['date'] && $_POST['description']){
         
-        $login = $_POST['login'];
-        $pass = $_POST['password'];
-        $confirmpass = $_POST['confirmpassword'];
+        $titre = $_POST['titre'];
+        $debut = $_POST['debut'];
+        $fin = $_POST['fin'];
+        $date = $_POST['date'];
+        $description = htmlspecialchars($_POST['description'],ENT_QUOTES);
 
         //si les passwords son identique 
-        if ($pass == $confirmpass){
-            
+        if ($debut != $fin){
 
-            //si login n'est pas le même
-            $log_ok = false;
-                //
-            foreach($request_fetch_all as $user){
-                if($login == $user [1]){
-                    $log_ok = false;
-                    //break;
-                }
-                else{$log_ok = true;}
-            }
-            
-            if($log_ok == true){
-
-                $sql = "INSERT INTO `reservation` (`login`,`password`) 
-                VALUE ('$login','$pass')";
-                $request2 = $mysqli -> query($sql);
-                //echo "ok";
-                header("location:connexion.php");
-
-            }
-            else{$erreur = '<p style = "color:red; font-weight:bold ">Le nom du login est déja utilisé</p>';}
-            
+            $sql = "INSERT INTO `reservations` (`titre`,`description`,`debut`,`fin`,`id_utilisateur`) 
+            VALUE ('$titre','$description','$date' '.$debut','$date' '.$fin',".$_SESSION['id'].")";
+            $request2 = $mysqli -> query($sql);
+            echo "ok";   
         }
-        else{$erreur = '<p style = "color:red; font-weight:bold ">les mots de passes ne sont pas identique</p>';}
+        else{$erreur = '<p style = "color:red; font-weight:bold ">les heures de début et de fin sont dentiques</p>';}
     }
     else{$erreur = '<p style = "color:red; font-weight:bold ">veuillez remplir tous les champs</p>';}
 }
@@ -81,32 +64,32 @@ if(isset($_POST['envoi'])){
 
                 <label for="debut">Heure de début</label>
                 <select name="debut">
-                    <option value="8h00">8h00</option>
-                    <option value="9h00">9h00</option>
-                    <option value="10h00">10h00</option>
-                    <option value="11h00">11h00</option>
-                    <option value="12h00">12h00</option>
-                    <option value="13h00">13h00</option>
-                    <option value="14h00">14h00</option>
-                    <option value="15h00">15h00</option>
-                    <option value="16h00">16h00</option>
-                    <option value="17h00">17h00</option>
-                    <option value="18h00">18h00</option>
+                    <option value="08:00">8h00</option>
+                    <option value="09:00">9h00</option>
+                    <option value="10:00">10h00</option>
+                    <option value="11:00">11h00</option>
+                    <option value="12:00">12h00</option>
+                    <option value="13:00">13h00</option>
+                    <option value="14:00">14h00</option>
+                    <option value="15:00">15h00</option>
+                    <option value="16:00">16h00</option>
+                    <option value="17:00">17h00</option>
+                    <option value="18:00">18h00</option>
                 </select>        
 
                 <label for="fin">Heure de fin</label>
                 <select name="fin">
-                    <option value="9h00">9h00</option>
-                    <option value="10h00">10h00</option>
-                    <option value="11h00">11h00</option>
-                    <option value="12h00">12h00</option>
-                    <option value="13h00">13h00</option>
-                    <option value="14h00">14h00</option>
-                    <option value="15h00">15h00</option>
-                    <option value="16h00">16h00</option>
-                    <option value="17h00">17h00</option>
-                    <option value="18h00">18h00</option>
-                    <option value="19h00">19h00</option>
+                    <option value="09:00">09h00</option>
+                    <option value="10:00">10h00</option>
+                    <option value="11:00">11h00</option>
+                    <option value="12:00">12h00</option>
+                    <option value="13:00">13:00</option>
+                    <option value="14:00">14h00</option>
+                    <option value="15:00">15h00</option>
+                    <option value="16:00">16h00</option>
+                    <option value="17:00">17h00</option>
+                    <option value="18:00">18h00</option>
+                    <option value="19:00">19h00</option>
                 </select> 
 
                 <label for="date">Date</label>
