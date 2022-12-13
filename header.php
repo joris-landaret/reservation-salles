e014bd0bffb113227843415d748ce7ebeedc4385
joris-landaret <joris.landaret@laplateforme.io>
2022-12-13 11:16:20 +0100

    ajout du lien du planning au header

--- header.php
+++ header.php
@@ -10,12 +10,12 @@
                 <?php if (!empty($_SESSION['login'])): ?>
                 <li><a href="http://reservationsalles/profil.php">Profil</a></li>
                 <li><a href="http://reservationsalles/deconnexion.php">Se déconnecter</a></li>
-                <li><a href="http://reservationsalles/commentaire">Commentaire</a></li>
+                <li><a href="http://reservationsalles/reservation-form.php">Réservation</a></li>
                 <?php else: ?>
                 <li><a href="http://reservationsalles/connexion.php">Connexion</a></li>
                 <li><a href="http://reservationsalles/inscription.php">Inscription</a></li>
                 <?php endif; ?>
-                <li><a href="http://reservationsalles/reservation-form.php">Réservation</a></li>
+                <li><a href="http://reservationsalles/planning.php">Planning</a></li>
             </ul>
         </div>
 
