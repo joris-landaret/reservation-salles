4f40c26364cd03edd487b54e761d5ac20312b7e1
joris-landaret <joris.landaret@laplateforme.io>
2022-12-16 17:34:10 +0100

    ajout des conditions pour aficher les reservations dans le tableau

--- planning.php
+++ planning.php
@@ -5,11 +5,14 @@ include('connect.php');
 
 $request = $mysqli -> query("SELECT * FROM reservations");
 
-$request_fetch_assoc = $request -> fetch_assoc();
+$request_fetch_assoc = $request -> fetch_all(MYSQLI_ASSOC);
+// $i=0;
+// while($request_fetch_assoc[$i]){
+// var_dump($request_fetch_assoc[$i]);
+// ;$i++;}
 
 //$request_fetch_all = $request -> fetch_array();
 
-var_dump($request_fetch_assoc);
 //echo ".$request_fetch_all[0]."
 
 // function table(int $col, int $row, int $week){
@@ -32,7 +35,7 @@ var_dump($request_fetch_assoc);
 //     $debut = $request_fetch_all['debut'];
 //     $fin = $request_fetch_all['fin'];
 //     $datetime_debut = new DateTime($debut);
-//     $datetime_fin = new DateTime($debut);
+//     $datetime_fin = new DateTime($fin);
 //     //$forma = format('d F Y à H:i');
 
 //     echo $datetime_debut->format('d F Y');
@@ -75,21 +78,59 @@ var_dump($request_fetch_assoc);
             
             <form action="" method="post">
 
-                <table border cellspacing="5" cellpadding="5" bgcolor="cyan"> 
+                <table border="10" cellspacing="5" cellpadding="5" bgcolor=""> 
                   
-                    <?php
-                    $days = ['lundi','mardi','mercredi','jeudi','vendredi'];
-                    $hours = ['7h','8h','9h','10h','11h','12h','13h','14h','15h','16h','17h','18h'];
-
-                    for ($i=0; $i < 12; $i++): ?>
-                        <tr>
-                            <td><?=$hours[$i]?></td>
-                        <?php for ($j=0; $j < 5; $j++) : ?>
-                            <td><?=$days[$j].' '.$hours[$i]?></td>
-                        
-                        <?php endfor; ?>
+                    <tr>
+                        <th></th>
+                        <th>Dimanche</th>
+                        <th>Lundi</th>
+                        <th>Mardi</th>
+                        <th>Mercredi</th>
+                        <th>Jeudi</th>
+                        <th>Vendredi</th>
                     </tr>
-                    <?php endfor; ?>
+
+                        
+
+                        <?php for ($i=0; $i < 11; $i++): ?>
+                        <?php   $days = ['dimanche','lundi','mardi','mercredi','jeudi','vendredi'];
+                                $hours = ['8h','9h','10h','11h','12h','13h','14h','15h','16h','17h','18h'];?>
+
+                                <tr>
+                                    <td><?=$hours[$i]?></td>
+                                
+                            <?php for ($j=0; $j < 6; $j++) : ?>
+
+                                <?php foreach($request_fetch_assoc as $reservation) : ?>
+                                    <?php //var_dump($reservation['debut']);
+
+                                        $debut = $reservation['debut'];
+                                        //$fin = $reservation['fin'];
+                                        $datetime_debut = new DateTime($debut);
+                                        //$datetime_fin = new DateTime($fin);
+                                        
+                                        //var_dump($datetime_debut->format('N G\h'));
+
+                                        $date_debut = $datetime_debut->format('N G\h');
+
+                                        $titre = $reservation['titre'];
+                                        //var_dump($titre);
+                                    ?>
+                        
+                                    <?php $date = $j.' '.$hours[$i];
+                                        //var_dump($date);
+                                        //var_dump($titre);
+                                    if ($date_debut == $date) : ?>
+                                        <td bgcolor="red"><?=$titre?></td>
+                                    <?php else: ?>
+                                        <td bgcolor="yellowgreen">libre</td>
+                                    <?php endif; ?>
+                                    <?php endforeach; ?>
+                            <?php endfor; ?>
+                            </tr>
+                        <?php endfor; ?>
+                    
+                    
                 </table>
             </form>
         </div>
@@ -99,19 +140,3 @@ var_dump($request_fetch_assoc);
 </body>
 
 </html>
-
-
-<?php 
-
-// for ($i=0; $i < 13; $i++) { 
-    
-//     <tr>
-//         for ($j=0; $j < 8; $j++) { 
-//            <td>test</td>
-//         }
-//         <td>test</td>
-//     </tr>
-
-// }
-
-?>
